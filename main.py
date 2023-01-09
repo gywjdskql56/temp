3133009a40307fe9ec6814ca1235a58199d48508
gywjdskql56 <ec2-user@ip-172-29-46-39.ap-northeast-2.compute.internal>
2023-01-09 04:00:46 +0000

    change file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import urllib
+import urllib.request as urllib
 import pandas as pd
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
@@ -10,12 +10,10 @@ def make_format(url, fileNM):
     keylist = ['거래금액','거래유형','건축년도','년','도로명','도로명건물본번호코드','도로명건물부번호코드','도로명시군구코드','도로명일련번호코드','도로명코드',
         '법정동','법정동본번코드','법정동부번코드','법정동시군구코드','법정동읍면동코드','법정동지번코드','아파트',
         '월','일','일련번호','전용면적','중개사소재지','지번','지역코드','층','해제사유발생일','해제여부']
-    res = urllib.request.urlopen(url)
+    res = urllib.urlopen(url)
     result = res.read()
     soup = BeautifulSoup(result, 'lxml-xml')
-    if soup.find('resultMsg').text == 'NORMAL SERVICE.':
-        print('EXCEED')
-    else:
+    if True:
         items = soup.findAll('item')
         itemList = []
         for v in items:
